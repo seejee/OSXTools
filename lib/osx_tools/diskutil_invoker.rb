9af2db6390813821d19525957cac52a1c9ebb45b
Chris Geihsler <chris@geihsler.net>
2011-05-10 23:53:53 -0400

    simplified a test

--- lib/osx_tools/diskutil_invoker.rb
+++ lib/osx_tools/diskutil_invoker.rb
@@ -22,12 +22,12 @@ module OsxTools
     private
 
     def query(verb, id=nil)
-      plist_xml = get_plist(verb, id)
+      args = build_query_args(verb, id)
+      plist_xml = get_plist(args)
       Plist.parse_xml(plist_xml)
     end
 
-    def get_plist(verb, id)
-      args = build_query_args(verb, id)
+    def get_plist(args)
       output = IO.popen(args)
       plist_xml = output.readlines.join(' ')
       output.close
