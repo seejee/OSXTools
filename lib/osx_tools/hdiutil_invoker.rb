8b7ae2a993508b07e41a7e84504ff0f36ea5935a
Chris Geihsler <chris@geihsler.net>
2011-05-15 12:16:44 -0400

    swallowing hdiutil output

--- lib/osx_tools/hdiutil_invoker.rb
+++ lib/osx_tools/hdiutil_invoker.rb
@@ -2,7 +2,7 @@ module OSXTools
   class HdiutilInvoker
 
     def mount_image(path_to_image)
-      succeeded = system('hdiutil', 'attach', path_to_image)
+      succeeded = `hdiutil attach #{path_to_image}`
       raise "Error mounting disk image" unless succeeded
     end
 
